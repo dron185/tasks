775d538bfee8c6e8a63c15592608d40e0cab5918
Andrei Huryn <gurin.nord@gmail.com>
2023-08-26 13:23:01 +0300

    feat: add carousel to section about

--- library/index.js
+++ library/index.js
@@ -22,8 +22,61 @@ console.log ('1. Вёрстка валидная +10\n2. Вёрстка сема
 
 }());
 
-<<<<<<< HEAD
 console.log ('1. Вёрстка соответствует макету. Ширина экрана 768px +26\n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n3. На ширине экрана 768рх реализовано адаптивное меню +12\nОценка: 50/50');
-=======
-console.log ('1. Вёрстка соответствует макету. Ширина экрана 768px +26\n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n3. На ширине экрана 768рх реализовано адаптивное меню +12\nОценка: 50/50');
->>>>>>> acb9ebd90be665a5e861c48baa7553d597e4a601
+
+// -------------------- About slider ----------------------------
+
+const cards = document.querySelector('.about__cards');
+const arrowLeft = document.querySelector('.about__arrow_left');
+const arrowRight = document.querySelector('.about__arrow_right');
+const dots = document.querySelectorAll('.about__radio-button');
+const buttons = document.querySelectorAll('.about__button');
+
+let position = 0;
+let dotIndex = 0;
+
+const nextSlide = () => {
+	if(position < 950 * 2) {
+		position += 475;
+		dotIndex++;
+	} else {
+		position = 1900;
+		dotIndex = (dots.length - 1);
+	}
+	cards.style.left = -position + 'px';
+	currentSlide(dotIndex);
+};
+
+const prevSlide = () => {
+	if(position > 0) {
+		position -= 475;
+		dotIndex--;
+	} else {
+		position = 0;
+		dotIndex = 0;
+	}
+	cards.style.left = -position + 'px';
+	currentSlide(dotIndex);
+};
+
+const currentSlide = (index) => {
+	for(let dot of buttons) {
+		dot.removeAttribute('checked')
+	}
+	buttons[index].setAttribute('checked', 'checked')
+}
+
+//eventlisteners
+arrowRight.addEventListener('click', nextSlide);
+arrowLeft.addEventListener('click', prevSlide);
+
+dots.forEach((dot, index) => {
+	dot.addEventListener('click', () => {
+		position = 475 * index
+		cards.style.left = -position + 'px';
+		dotIndex = index;
+		currentSlide(dotIndex);
+	})
+});
+
+

2b98fa0d1f64b2d602346a3c2845eadc947ec8f6
Andrei Huryn <gurin.nord@gmail.com>
2023-09-29 10:06:16 +0300

    feat: get images to page

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,47 +1,32 @@
 const button = document.querySelector('.header__search-btn');
 const search = document.getElementById('search');
 const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=Xag3asSOqMZBOXZBV9SEB3rYrmIYSBY46IMEjTzLDv8';
-const imgBox = document.querySelector('.gallery__box');
-// const image = document.querySelector('.gallery__img');
-
-
-// async function getData() {
-// 	try {
-// 		const response = await fetch(url);
-// 		const data = await response.json();
-// 		console.log(data);
-// 		// console.log(data.results[0].urls.regular);
-// 		showData(data);
-// 		// imgBox.style.backgroundImage = "url('data.results[0].urls.regular')";
-// 		// image.src = data.results[0].urls.regular;
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
-// getData();
-
-// function showData(data) {
-// 	// imgBox.style.backgroundImage = "url('data.results[0].urls.regular')";
-// 	image.src = data.results[0].urls.regular;
-// }
-
-const img = document.createElement('img');
-img.classList.add('gallery__img');
-img.alt = `image`;
-imgBox.append(img);
+const gallery = document.querySelector('.gallery');
 
 async function getData() {
 	const response = await fetch(url);
 	const data = await response.json();
 	console.log(data);
-	showData(data);
+
+	const results = data.results;
+	console.log(results);
+
+	for (let item of results.values()) {
+		let imgUrl = item.urls.regular;
+		console.log(imgUrl);
+		showData(imgUrl);
+	}
 }
 getData();
 
-function showData(data) {
-	img.src = data.results[0].urls.regular;
+function showData(imgUrl) {
+	const img = document.createElement('img');
+	img.classList.add('gallery__img');
+	img.alt = `image`;
+	const imgBox = document.createElement('div');
+	imgBox.classList.add('gallery__box');
+	imgBox.append(img);
+	img.src = imgUrl;
+	gallery.append(imgBox);
 }
-
-
-
 

e437a63be52809a8cc4b34a4e6bc25872472fc4a
Andrei Huryn <gurin.nord@gmail.com>
2023-10-05 22:19:38 +0300

    feat: add animals animation

--- random-game/index.js
+++ random-game/index.js
@@ -2,24 +2,57 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d'); // указываем формат нашей игры (двумерный контекст)
 
 const background = document.createElement('img');
-const monkey = document.createElement('img');
-const monkeyBottom = document.createElement('img');
 const monkeyCorner = document.createElement('img');
+const monkeyHang = document.createElement('img');
+const snake = document.createElement('img');
 const bird = document.createElement('img');
 
+// bird's positions
+let birdX = 450;
+let birdY = 250;
+// bird's change positions
+let newY = 3;
+// the gap between animals
+let distance = 200;
+
 background.src = 'assets/img/bg.png';
-monkey.src = 'assets/img/monkey1.png';
-monkeyBottom.src = 'assets/img/monkey2.png';
 monkeyCorner.src = 'assets/img/monkey3.png';
+monkeyHang.src = 'assets/img/monkey4.png';
+snake.src = 'assets/img/snake.png';
 bird.src = 'assets/img/bird.png';
 
-function draw() {
-	context.drawImage(background, 0, 0);
-	context.drawImage(monkey, 500, -90);
-	context.drawImage(monkeyBottom, 520, canvas.height - monkeyBottom.height);
-	context.drawImage(monkeyCorner, 0, canvas.height - monkeyBottom.height);
-	context.drawImage(bird, 250, 350);
-
+let animalsArray = []; 
+animalsArray[0] = {
+	posX : canvas.width,
+	posY : 200
 }
 
-bird.onload = draw;
+bird.onload = function draw() {
+	context.drawImage(background, 0, 0);
+
+	for (let i = 0; i < animalsArray.length; i++) {
+		context.drawImage(monkeyHang, animalsArray[i].posX, -animalsArray[i].posY);
+		context.drawImage(snake, animalsArray[i].posX + 100, monkeyHang.height + distance - animalsArray[i].posY );
+		animalsArray[i].posX -= 1;
+		// console.log(animalsArray[i].posX);
+		if (animalsArray[i].posX == 200) {
+			animalsArray.push({
+				posX : canvas.width,
+				posY : Math.floor(Math.random() * snake.height)
+			});
+		} 
+		// else if (animalsArray.length > 2) {
+		// 	animalsArray.pop();
+		// }
+		console.log(animalsArray.length);
+	}
+
+	context.drawImage(monkeyCorner, 0, canvas.height - monkeyCorner.height);
+	context.drawImage(bird, birdX, birdY);
+	birdY += newY;
+	requestAnimationFrame(draw);
+}
+
+document.addEventListener('keydown', () => {
+	birdY -= 60;
+})

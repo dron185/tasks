1da2b61391ecdea22f3c4957a26ec097ac63c101
Andrei Huryn <gurin.nord@gmail.com>
2023-10-06 18:05:26 +0300

    feat: add game over after impact

--- random-game/index.js
+++ random-game/index.js
@@ -8,12 +8,12 @@ const snake = document.createElement('img');
 const bird = document.createElement('img');
 
 // bird's positions
-let birdX = 450;
+let birdX = 600;
 let birdY = 250;
 // bird's change positions
-let newY = 3;
-// the gap between animals
-let distance = 200;
+let newY = 1.5;
+// the distance between animals
+let distance = 300;
 
 background.src = 'assets/img/bg.png';
 monkeyCorner.src = 'assets/img/monkey3.png';
@@ -28,28 +28,37 @@ animalsArray[0] = {
 }
 
 bird.onload = function draw() {
+	let score = animalsArray.length - 1;
 	context.drawImage(background, 0, 0);
+	context.drawImage(monkeyCorner, 0, canvas.height - monkeyCorner.height);
+	context.drawImage(bird, birdX, birdY);
+	birdY += newY;
 
 	for (let i = 0; i < animalsArray.length; i++) {
 		context.drawImage(monkeyHang, animalsArray[i].posX, -animalsArray[i].posY);
 		context.drawImage(snake, animalsArray[i].posX + 100, monkeyHang.height + distance - animalsArray[i].posY );
 		animalsArray[i].posX -= 1;
-		// console.log(animalsArray[i].posX);
-		if (animalsArray[i].posX == 200) {
+		if (animalsArray[i].posX == 300 ) { 
 			animalsArray.push({
 				posX : canvas.width,
 				posY : Math.floor(Math.random() * snake.height)
 			});
 		} 
-		// else if (animalsArray.length > 2) {
-		// 	animalsArray.pop();
-		// }
-		console.log(animalsArray.length);
-	}
+		
+		context.fillStyle = 'azure';
+		context.font = '52px Caveat Brush';
+		context.fillText('Score: ' + score, 20, 45);
 
-	context.drawImage(monkeyCorner, 0, canvas.height - monkeyCorner.height);
-	context.drawImage(bird, birdX, birdY);
-	birdY += newY;
+		// при касании птицей других животных и земли - перезагрузка
+		if (birdX + bird.width >= animalsArray[i].posX + 100 && birdX <= animalsArray[i].posX + monkeyHang.width -100 && 
+			(birdY <= -animalsArray[i].posY + monkeyHang.height || birdY + bird.height >= -animalsArray[i].posY + monkeyHang.height + distance) 
+			|| birdY + bird.height >= canvas.height) {
+
+			alert("GAME OVER!");
+			document.location.reload();
+			clearInterval(interval);
+		}
+	}
 	requestAnimationFrame(draw);
 }
 

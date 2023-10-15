60c0bc1203493904caa2d321f275ec7bea12196d
Andrei Huryn <gurin.nord@gmail.com>
2023-10-15 18:33:45 +0300

    feat: add results after win and game over

--- random-game/index.js
+++ random-game/index.js
@@ -28,7 +28,12 @@ animalsArray[0] = {
 }
 let score = 0;
 
+const modalTitle = document.querySelector('.modal__title');
+const modalScore = document.querySelector('.modal__score');
+const modalRes = document.querySelector('.modal__results');
+
 function draw() {
+	let animation = requestAnimationFrame(draw);
 	context.drawImage(background, 0, 0);
 	context.drawImage(monkeyCorner, 0, canvas.height - monkeyCorner.height);
 	context.drawImage(bird, birdX, birdY);
@@ -36,76 +41,68 @@ function draw() {
 
 	let scoreArray = localStorage.getItem('myScore') ? JSON.parse(localStorage.getItem('myScore')) : [];
 	localStorage.setItem('myScore', JSON.stringify(scoreArray));
-
-	showScore();
-
-	animalsArray.forEach(element => {
-		if (element.posX == 250) {
-			score++;
-		}
-	});
-
-	let animation = requestAnimationFrame(draw);
+	let isGame = true;
+	
 	for (let i = 0; i < animalsArray.length; i++) {
 		context.drawImage(monkeyHang, animalsArray[i].posX, -animalsArray[i].posY);
 		context.drawImage(snake, animalsArray[i].posX + 100, monkeyHang.height + distance - animalsArray[i].posY );
 		animalsArray[i].posX -= 1;
-		if ( animalsArray[i].posX == 800 ) {  // or 700
+		
+		if ( animalsArray[i].posX == 800 ) {
 			animalsArray.push({
 				posX : canvas.width,
 				posY : Math.floor(Math.random() * snake.height)
 			});
 		}
+		
+		// добавляем очки при прохождении animals позиции 250
+		if ( animalsArray[i].posX == 250 ) {
+			score++;
+		}
 
-		// при касании птицей других животных и земли - перезагрузка
+		// при касании птицей других животных и земли - game over
 		if (birdX + bird.width >= animalsArray[i].posX + 100 && birdX <= animalsArray[i].posX + monkeyHang.width -130 && 
 			(birdY <= -animalsArray[i].posY + monkeyHang.height || birdY + bird.height >= -animalsArray[i].posY + monkeyHang.height + distance) 
 			|| birdY + bird.height >= canvas.height) {
-			scoreArray.push(score);
-			localStorage.setItem('myScore', JSON.stringify(scoreArray));
+
+			isGame = false;
+			modalTitle.innerHTML = 'GAME OVER!';
+			modalScore.innerHTML = score;
 
 			document.querySelector('.modal').classList.add('open');
 			document.querySelector('.modal__button').addEventListener('click', () => {
 				document.querySelector('.modal').classList.remove('open');
 				document.location.reload();
 			})
-			// alert("GAME OVER!");
-			// document.location.reload();
-			
 			cancelAnimationFrame(animation);
-			// clearInterval(animation);
+		}
+	
+		// при достижении 5 очков - победа
+		if (score == 5) {
+			isGame = false;
+			modalTitle.innerHTML = 'YOU WON! Congratulations!';
+			modalScore.innerHTML = score;
+
+			document.querySelector('.modal').classList.add('open');
+			document.querySelector('.modal__button').addEventListener('click', () => {
+				document.querySelector('.modal').classList.remove('open');
+				document.location.reload();
+			})
+			cancelAnimationFrame(animation);
 		}
 	}
+	if (!isGame) {
+		scoreArray.push(score);
+		modalRes.innerHTML = scoreArray.slice(-10);  // results of the last 10 games
+		localStorage.setItem('myScore', JSON.stringify(scoreArray));
+	}
 	
-}
-bird.onload = draw;
-
-scoreArray = JSON.parse(localStorage.getItem("myScore"));
-console.log(scoreArray)
-
-// best score and score of the last 10 games
-let bestScore;
-let totalResult;
-if (scoreArray == null) {
-	bestScore = 0;
-	totalResult = 0;
-} else {
-	bestScore = Math.max.apply(null, scoreArray);
-	totalResult = scoreArray.slice(-10);
-}
-
-function showScore() {
 	context.fillStyle = '#ffffff';
 	context.font = '52px Caveat Brush';
 	context.fillText('Score: ' + score, 20, 45);
-	context.fillText('Best score: ' + bestScore, 190, 45);
 }
+bird.onload = draw;
 
 document.addEventListener('keydown', () => {
 	birdY -= 60;
 })
-
-// results of the last 10 games
-const modalRes = document.querySelector('.modal__results');
-console.log(totalResult);
-modalRes.innerHTML = totalResult;
